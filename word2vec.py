7cb206d5acf04238a35345eea263e7d6b1bcfdd4
Peter Langballe Erichsen <perich16@student.aau.dk>
2020-09-14 10:41:51 +0200

    began preprocessing

--- word2vec.py
+++ word2vec.py
@@ -4,36 +4,50 @@ import json
 from multiprocessing import Pool
 from tqdm import tqdm
 
-# ID, word, count
+# formatted as: ID, header, body
 documents = []
 # name of file to load words from
 load_filename = "documents.json"
 # name of file to save down
 save_filename = "word2vec.txt"
 pool_size = 4
+word_minimum_count = 20
+word_maximum_percent = 25
 
 
 def main():
     print('Beginning Word2Vec Procedure.')
+
+    # load documents file
     print('Beginning to load documents from "' + load_filename + '".')
     load_file(load_filename)
     print('Finished Loaded Word2Vec from "' + load_filename + '".')
 
-    word2vec = {}
-    word_counts = []
-    with Pool(pool_size) as p:
-        word_counts = p.map(word2vec_doc_load, documents)
-    for wcs in tqdm(word_counts):
-        for wc in wcs:
-            word2vec[wc[0]] = word2vec.get(wc[0], 0) + wc[1]
-    print('Found ' + str(len(word2vec)) + " unique words.")
+    # transform documents into a dict containing unique word counts
+    word_count_dict = word_to_vec()
+    print('Found ' + str(len(word_count_dict)) + " unique words.")
 
+    # preprocess to remove unhelpful words
+    word_count_dict = {key: word_count_dict[key] for key in word_count_dict if word_count_dict[key] < 20}
+    print('Found ' + str(len(word_count_dict)) + " unique words.")
+
+    # save word2vec file
     print('Beginning to save Word2Vec in "' + save_filename + '".')
-    save_file(save_filename, word2vec)
+    save_file(save_filename, word_count_dict)
     print('Finished Saved Word2Vec in "' + save_filename + '".')
 
     print('Finished Word2Vec Procedure.')
     return
+
+
+def word_to_vec():
+    word_count_dict = {}
+    with Pool(pool_size) as p:
+        word_counts = p.map(word2vec_doc_load, documents)
+        for wcs in tqdm(word_counts):
+            for wc in wcs:
+                word_count_dict[wc[0]] = word_count_dict.get(wc[0], 0) + wc[1]
+    return word_count_dict
 
 
 def load_file(filename):
