4a15bbec9293900bcce0caf0ad080a2f1b65cb2c
Peter Langballe Erichsen <perich16@student.aau.dk>
2020-09-15 09:47:24 +0200

    seperated functions more.
    also made parameters have standard values, but be part of the function parameters.

--- preprocessing.py
+++ preprocessing.py
@@ -1,31 +1,18 @@
-import re
 import json
-import numpy as np
+import re
 
-from multiprocessing import Pool
-from tqdm import tqdm
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.pipeline import Pipeline
-
-# formatted as: ID, header, body
-documents = {}
-corpus = []
-# name of file to load words from
-load_filename = "documents.json"
-# name of file to save down
-word_save_filename = "word2vec.txt"
-doc_save_filename = "doc2vec.txt"
-pool_size = 4
-word_minimum_count = 20
-word_maximum_doc_percent = 0.25
-doc_minimum_length = 20
+from tqdm import tqdm
 
 
-def main(save):
+def preprocess(load_filename="documents.json", word_save_filename="word2vec.txt", doc_save_filename="doc2vec.txt",
+               word_minimum_count=20, word_maximum_doc_percent=0.25, doc_minimum_length=20, save=True):
     print('Beginning Word2Vec Procedure.')
 
     # load documents file
-    load_file(load_filename)
+    documents = load_file(load_filename)
+    # filter documents and create corpus
+    documents, corpus = filter_documents(documents, doc_minimum_length)
 
     # transform documents into a matrix containing counts for each word in each document
     # also cut off words that are used too often or too little (max/min document frequency)
@@ -41,36 +28,51 @@ def main(save):
     if save:
         print('Saving word and document lookup files.')
         save_file(word_save_filename, words)
-        save_file(doc_save_filename, documents.values())
+        save_file(doc_save_filename, documents.keys())
     return X
 
 
 def load_file(filename):
-    print('Beginning to load documents from "' + load_filename + '".')
+    print('Loading documents from "' + filename + '".')
+    documents = {}
     with open(filename, "r", encoding="utf-8") as json_file:
-        index = 0
         for json_obj in json_file:
             data = json.loads(json_obj)
-            text = data['headline'] + " " + data['body']
-            text = text.lower()
-            text = re.findall(r"[a-zæøå]+", text)
-            # Remove documents containing too few words.
-            if len(text) < doc_minimum_length:
-                continue
-            text = ' '.join(text)
-            corpus.append(text)
-            documents[index] = data['id']
-            index += 1
-    print('Loaded ' + str(len(documents)) + 'valid documents.')
+            documents[data['id']] = data['headline'] + ' ' + data['body']
+    print('Loaded ' + str(len(documents)) + ' documents.')
+    return documents
 
 
-def save_file(filename, words):
+def filter_documents(documents, doc_minimum_length):
+    print('Filtering documents.')
+    corpus = []
+    bad_docs = []
+    for doc in tqdm(documents.items()):
+        text = doc[1]
+        text = text.lower()
+        text = re.findall(r"[a-zæøå]+", text)
+        # Remove documents containing too few words.
+        if len(text) < doc_minimum_length:
+            bad_docs.append(doc)
+            continue
+        text = ' '.join(text)
+        documents[doc[0]] = text
+        corpus.append(text)
+    for doc in bad_docs:
+        del documents[doc[0]]
+    print('Filtering documents. ' + str(len(documents)) + ' remaining')
+    return documents, corpus
+
+
+def save_file(filename, content):
+    print('Saving file "' + filename + '".')
     with open(filename, "w") as file:
         id_counter = 0
-        for w in words:
-            file.write(str(id_counter) + ", " + str(w) + '\n')
+        for c in content:
+            file.write(str(id_counter) + ", " + str(c) + '\n')
             id_counter += 1
+    print('"' + filename + '" has been saved.')
 
 
-if __name__ == "__main__":
-    main(True)
+if __name__ == '__main__':
+    preprocess()
