24d07793579a7dcc1477b571aafe526938b89cce
Simba <rech16@student.aau.dk>
2020-09-22 10:41:16 +0200

    in need of a good way to save adj matrix

--- graph.py
+++ graph.py
@@ -1,19 +1,25 @@
 import math
-from typing import Dict
+from multiprocessing import Pool
+from typing import Dict, List
+
+import h5py as h5py
 import numpy as np
 import networkx as net
+import sklearn
+from gensim.corpora import Dictionary
 from gensim.models import LdaModel
+from tqdm import tqdm
+import itertools
+import json
+import scipy.sparse as sp
+import ast
 
-from LDA.lda import get_document_topics_from_model
+from database import HDF5Store
 from preprocessing import load_document_file, preprocess
 
 
-def similarity_between_documents(d1: Dict[int, float], d2: Dict[int, float], num_of_topics: int):
-    similarity = 0
-    for number in range(num_of_topics):
-        if number in d1 and number in d2:
-            similarity += min(d1[number], d2[number])
-    return similarity
+def similarity_between_documents(d1: Dict[int, float], d2: Dict[int, float]):
+    return sum([min(d1[number], d2[number]) for number in set(d1) & set(d2)])
 
 
 def construct_transistion_probability_matrix(graph: net.Graph) -> np.ndarray:
@@ -28,26 +34,67 @@ def construct_transistion_probability_matrix(graph: net.Graph) -> np.ndarray:
     return np.array(row_normalized_matrix, dtype=np.float64)
 
 
-def document_graph(path_to_file: str) -> net.graph:
-    documents = preprocess(path_to_file)
-    lda_model = LdaModel.load("LDA/model/docu_model")
+def get_document_topics_from_model(text: str):
+    dictionary = Dictionary([text])
+    corpus = [dictionary.doc2bow(t) for t in [text]]
 
-    documents_topics = [get_document_topics_from_model(lda_model, x) for x in documents]
+    query = lda_model.get_document_topics(corpus)
 
-    document_graph = net.Graph()
+    return dict([x for x in query][0])
 
-    # iterating over sentences and adding them as nodes
-    # comparing all sentences to each other.
-    for document in documents:
-        document_graph.add_node(document)
-        for second_document in documents:
-            similarity = similarity_between_documents(
-                get_document_topics_from_model(lda_model, document),
-                get_document_topics_from_model(lda_model, second_document),
-                lda_model.num_topics)
-            document_graph.add_edge(document, second_document, weight=similarity)
-    return document_graph
+
+def load_document_topics(path: str):
+    with open(path, 'r') as file:
+        document_topics = json.load(file)
+    return [ast.literal_eval(x) for x in document_topics]
+
+
+def create_document_topics(corpus):
+    document_topics = []
+    with Pool(8) as p:
+        document_topics.append(p.map(get_document_topics_from_model, corpus))
+    with open('document_topics.json', 'w') as file:
+        json.dump(document_topics, file)
+    return document_topics
+
+
+def save_topic_doc_matrix(document_topics: List[Dict[int, float]]):
+    matrix = sp.dok_matrix((len(document_topics), lda_model.num_topics))
+    for index, dictionary in enumerate(document_topics):
+        for dict_key, dict_value in dictionary.items():
+            matrix[index, dict_key] = dict_value
+
+    return sp.save_npz("Generated Files/topic_doc_matrix", sp.csc_matrix(matrix))
+
+
+def document_graph(document_topics=None) -> net.graph:
+    if document_topics is None:
+        document_topics = load_document_topics("document_topics.json")
+
+    doc_sim_matrix = sp.dok_matrix((len(document_topics), len(document_topics)))
+    for index, document in tqdm(enumerate(document_topics)):
+        for second_index in range(index):
+            sim = similarity_between_documents(document_topics[index], document_topics[second_index])
+            doc_sim_matrix[index, second_index] = sim
+    sp.save_npz("Generated Files/doc_sim_matrix", doc_sim_matrix)
+    return doc_sim_matrix
+
+
+def parallel(index, document):
+    for second_index in range(index):
+        sim = similarity_between_documents(document_topics[index], document_topics[second_index])
+        doc_sim_matrix[index, second_index] = sim
+
+
+def parallel_doc_sim_matrix(document_topics):
+    with Pool(8) as p:
+        p.starmap(parallel, [x for x in enumerate(document_topics)])
 
 
 if __name__ == '__main__':
-    graph = document_graph("documents.json")
+    # Loading stuff
+    document_topics = load_document_topics("document_topics.json")
+    lda_model = LdaModel.load("LDA/model/docu_model")
+    doc_sim_matrix = sp.dok_matrix((len(document_topics), len(document_topics)))
+
+    parallel_doc_sim_matrix(document_topics)
