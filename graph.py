a7485c936292891b12fa938442d8b01f9c676635
Simba <rech16@student.aau.dk>
2020-09-23 10:38:58 +0200

    some documentation

--- graph.py
+++ graph.py
@@ -1,11 +1,7 @@
 from multiprocessing import Pool
-import ast
-import json
-from multiprocessing import Pool
 from typing import Dict, List
 
 import networkx as net
-import numpy as np
 import scipy.sparse as sp
 from gensim.corpora import Dictionary
 from gensim.models import LdaModel
@@ -14,53 +10,65 @@ from tqdm import tqdm
 from preprocessing import preprocess
 
 
-def similarity_between_documents(d1: Dict[int, float], d2: Dict[int, float]):
-    return sum([min(d1[number], d2[number]) for number in set(d1) & set(d2)])
-
-
-def load_document_topics(path: str):
-    with open(path, 'r') as file:
-        document_topics = json.load(file)
-    return [ast.literal_eval(x) for x in document_topics][0]
+def similarity_between_documents(d1: int, d2: int):
+    """
+    Computes similarity between two documents by summing the minimum of their topic distribution.
+    :param d1: document 1
+    :param d2: document 2
+    :return: a similarity value between 0 and 1
+    """
+    return sum([min(topic_doc_matrix.getrow(d1).toarray()[0][number], topic_doc_matrix.getrow(d2).toarray()[0][number]) for number in set(topic_doc_matrix.getrow(0).nonzero()[1]) & set(topic_doc_matrix.getrow(1).nonzero()[1])])
 
 
 def create_document_topics(corpus: List[str]):
+    """
+    Creates a topic_doc_matrix which describes the amount of topics in each document
+    :param corpus: list of document strings
+    :return: a topic_document matrix
+    """
     document_topics = []
     with Pool(8) as p:
         document_topics.append(p.map(get_document_topics_from_model, corpus))
-    with open('document_topics.json', 'w') as file:
-        json.dump([str(document_topics[0])], file)
-    return document_topics
+    matrix = save_topic_doc_matrix(document_topics[0])
+    return matrix
 
 
 def get_document_topics_from_model(text: str):
+    """
+    A method used concurrently in create_document_topics
+    :param text: a document string
+    :return: a dict with the topics in the given document based on the lda model
+    """
     tokenized_text = [text.split(' ')]
-
     dictionary = Dictionary(tokenized_text)
     corpus = [dictionary.doc2bow(t) for t in tokenized_text]
-
     query = lda_model.get_document_topics(corpus, minimum_probability=0.025)
-
     return dict([x for x in query][0])
 
 
 def save_topic_doc_matrix(document_topics: List[Dict[int, float]]):
+    """
+    Saves the document topics (list of dicts) in a matrix
+    :param document_topics: list of dicts
+    :return: a matrix (scipy)
+    """
     matrix = sp.dok_matrix((len(document_topics), lda_model.num_topics))
-    for index, dictionary in enumerate(document_topics):
+    for index, dictionary in tqdm(enumerate(document_topics)):
         for dict_key, dict_value in dictionary.items():
             matrix[index, dict_key] = dict_value
+    sp.save_npz("Generated Files/topic_doc_matrix", sp.csc_matrix(matrix))
+    return matrix
 
-    return sp.save_npz("Generated Files/topic_doc_matrix", sp.csc_matrix(matrix))
 
-
-def document_graph(document_topics=None) -> net.graph:
-    if document_topics is None:
-        document_topics = load_document_topics("document_topics.json")
-
-    doc_sim_matrix = sp.dok_matrix((len(document_topics), len(document_topics)))
-    for index, document in tqdm(enumerate(document_topics)):
+def document_similarity_matrix(matrix) -> net.graph:
+    """
+    Todo still not optimized
+    """
+    length = matrix.shape[0]
+    doc_sim_matrix = sp.dok_matrix((length, length))
+    for index in tqdm(range(length)):
         for second_index in range(index):
-            sim = similarity_between_documents(document_topics[index], document_topics[second_index])
+            sim = similarity_between_documents(index, second_index)
             doc_sim_matrix[index, second_index] = sim
     sp.save_npz("Generated Files/doc_sim_matrix", doc_sim_matrix)
     return doc_sim_matrix
@@ -79,12 +87,8 @@ def document_graph(document_topics=None) -> net.graph:
 
 if __name__ == '__main__':
     # Loading stuff and initialisation
-    # document_topics = load_document_topics("document_topics.json")
-    # doc_sim_matrix = sp.dok_matrix((len(document_topics), len(document_topics)))
+    topic_doc_matrix = sp.load_npz("Generated Files/topic_doc_matrix.npz")
     lda_model = LdaModel.load("LDA/model/docu_model")
 
     # documents = preprocess('documents.json')
     # create_document_topics(documents[2])
-    documet_topics = load_document_topics('document_topics.json')
-    print("hello")
-    # parallel_doc_sim_matrix(document_topics)
