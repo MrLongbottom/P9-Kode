8a3c73aba9e6cab6ed98c72f48339ddd94a43040
Peter Langballe Erichsen <perich16@student.aau.dk>
2020-09-25 14:35:19 +0200

    setup to make other similarity function

--- graph.py
+++ graph.py
@@ -34,6 +34,28 @@ def document_similarity_matrix(matrix) -> net.graph:
     return doc_sim_matrix
 
 
+def document_similarity_matrix_xyz(td_matrix):
+    sim = {}
+    for doc_id in tqdm(range(td_matrix.shape[0])):
+        doc = td_matrix.getrow(doc_id)
+        topics_in_doc = doc.nonzero()[1]
+        for topic_id in topics_in_doc:
+            topic = td_matrix.getcol(topic_id)
+            docs_in_topic = topic.nonzero()[0]
+            Y = {x: topic[x].data[0] for x in docs_in_topic if x != doc_id}
+            #filter docs that have already been done, .ie if sim(x,y) check if sim(y,x) already is in sim.
+            x = topic[doc_id].data[0]
+            test = sum_similarity(x, Y)
+            for key, val in test.items():
+                sim[doc_id, key] = sim.get((doc_id, key), 0) + val
+    return sim
+
+def sum_similarity (x, Y):
+    dict = {}
+    for id, y in Y.items():
+        dict[id] = min(x, y)
+    return dict
+
 # def parallel(index, document):
 #     for second_index in range(index):
 #         sim = similarity_between_documents(document_topics[index], document_topics[second_index])
@@ -49,6 +71,6 @@ if __name__ == '__main__':
     # Loading stuff and initialisation
     topic_doc_matrix = sp.load_npz("Generated Files/topic_doc_matrix.npz")
     lda_model = LdaModel.load("LDA/model/docu_model")
-
+    document_similarity_matrix_xyz(topic_doc_matrix)
     # documents = preprocess('documents.json')
     # create_document_topics(documents[2])
