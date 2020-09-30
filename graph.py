4dc243157f386d915c7d1ff554d6544211b1eab4
Simba <rech16@student.aau.dk>
2020-09-30 11:02:45 +0200

    initial saving function

--- graph.py
+++ graph.py
@@ -1,9 +1,11 @@
+from functools import partial
 from multiprocessing import Pool
 
-import h5py
 import networkx as net
 import scipy.sparse as sp
 from tqdm import tqdm
+
+from preprocessing import preprocess
 
 
 def similarity_between_documents(d1: int, d2: int):
@@ -14,8 +16,8 @@ def similarity_between_documents(d1: int, d2: int):
     :return: a similarity value between 0 and 1
     """
     return sum(
-        [min(topic_doc_matrix.getrow(d1).toarray()[0][number], topic_doc_matrix.getrow(d2).toarray()[0][number]) for
-         number in set(topic_doc_matrix.getrow(0).nonzero()[1]) & set(topic_doc_matrix.getrow(1).nonzero()[1])])
+        [min(matrix.getrow(d1).toarray()[0][number], matrix.getrow(d2).toarray()[0][number]) for
+         number in set(matrix.getrow(0).nonzero()[1]) & set(matrix.getrow(1).nonzero()[1])])
 
 
 def document_similarity_matrix(matrix) -> net.graph:
@@ -32,61 +34,72 @@ def document_similarity_matrix(matrix) -> net.graph:
     return doc_sim_matrix
 
 
-def document_similarity_matrix_xyz(td_matrix):
-    iterable = [(i, x) for i, x in enumerate(td_matrix)]
-    with Pool(8) as p:
-        p.map(document_sim_matrix_par, iterable)
+# def document_similarity_matrix_xyz(td_matrix):
+#     iterable = [(i, x) for i, x in enumerate(td_matrix)]
+#     with Pool(8) as p:
+#         p.map(document_sim_matrix_par, iterable)
+#
 
 
-def document_sim_matrix_par(pair):
-    doc_id = pair[0]
-    doc = pair[1]
+def document_sim_matrix_par(td_matrix, doc_id):
+    doc = td_matrix.getrow(doc_id)
     topics_in_doc = doc.nonzero()[1]
     skips = 0
     sim_dict = {}
-    with h5py.File('adj.hdf5', 'a') as file:
-        skipable = file['data'][doc_id].any()
-    if not skipable:
-        for topic_id in topics_in_doc:
-            topic = td_matrix.getcol(topic_id)
-            docs_in_topic = topic.nonzero()[0]
-            len1 = len(docs_in_topic)
-            # filter docs that have already been done, .ie documents earlier in the loop
-            docs_in_topic = [x for x in docs_in_topic if doc_id <= x]
-            len2 = len(docs_in_topic)
-            if len1 != len2:
-                skips += len1 - len2
-            Y = {x: topic[x].data[0] for x in docs_in_topic if doc_id <= x}
-            x = topic[doc_id].data[0]
-            similarity_sum = {id: min(x, y) for id, y in Y.items()}
-            for key, val in similarity_sum.items():
-                sim_dict[doc_id, key] = sim_dict.get((doc_id, key), 0) + val
-            with h5py.File('adj.hdf5', 'a') as file:
-                file['data'][doc_id].put(list(sim_dict.keys()), list(sim_dict.values()))
-        print(f"Doc: {doc_id} Skipped: {skips}")
+    for topic_id in topics_in_doc:
+        topic = td_matrix.getcol(topic_id)
+        docs_in_topic = topic.nonzero()[0]
+        len1 = len(docs_in_topic)
+        # filter docs that have already been done, .ie documents earlier in the loop
+        docs_in_topic = [x for x in docs_in_topic if doc_id <= x]
+        len2 = len(docs_in_topic)
+        if len1 != len2:
+            skips += len1 - len2
+        Y = {x: topic[x].data[0] for x in docs_in_topic if doc_id <= x}
+        x = topic[doc_id].data[0]
+        similarity_sum = {id: min(x, y) for id, y in Y.items()}
+        for key, val in similarity_sum.items():
+            sim_dict[doc_id, key] = sim_dict.get((doc_id, key), 0) + val
+    print(f"Doc: {doc_id} Skipped: {skips}")
+    return sim_dict
 
 
-# def parallel(index, document):
+def document_similarity_matrix_xyz(td_matrix, chunks):
+    for index, chunk in enumerate(chunks):
+        sim = {}
+        with Pool(8) as p:
+            doc_sim = p.map(partial(document_sim_matrix_par, td_matrix), chunk)
+        for dictionary in tqdm(doc_sim):
+            sim.update(dictionary)
+        dok = sp.dok_matrix((td_matrix.shape[0], td_matrix.shape[0]))
+        for (a, b), v in sim.items():
+            dok[a, b] = v
+        sp.save_npz(f"Generated Files/adj_matrix{index}", sp.csr_matrix(dok))
+
+
+# def parallel(index):
 #     for second_index in range(index):
-#         sim = similarity_between_documents(document_topics[index], document_topics[second_index])
-#         doc_sim_matrix[index, second_index] = sim
+#         doc_sim_matrix[index, second_index] = similarity_between_documents(index, second_index)
 #
 #
-# def parallel_doc_sim_matrix(document_topics):
+# def parallel_doc_sim_matrix(matrix):
 #     with Pool(8) as p:
-#         p.starmap(parallel, [x for x in enumerate(document_topics)])
+#         max_ = matrix.shape[0]
+#         with tqdm(total=max_) as pbar:
+#             for i, _ in enumerate(p.imap_unordered(parallel, list(range(matrix.shape[0])))):
+#                 pbar.update()
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
 
 if __name__ == '__main__':
     # Loading stuff and initialisation
-    td_matrix = sp.load_npz("Generated Files/topic_doc_matrix.npz")
-    adj_matrix = sp.dok_matrix((td_matrix.shape[0], td_matrix.shape[0]))
-    with h5py.File('adj.hdf5', 'a') as file:
-        if not file['data']:
-            file.create_dataset("data", adj_matrix.shape)
+    matrix = sp.load_npz("Generated Files/count_vec_matrix.npz")
+    # document_similarity_matrix_xyz(td_matrix)
+    chunks = chunks(range(matrix.shape[0]), 100)
 
-    with h5py.File('adj.hdf5', 'a') as file:
-        skipable = file['data'].any()
-    document_similarity_matrix_xyz(td_matrix)
-    # documents = preprocess('documents.json')
-    # create_document_topics(documents[2])
+    document_similarity_matrix_xyz(matrix, chunks)
