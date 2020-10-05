68254f5a2067aa2854db487e4efbd0f2ef5b7cdb
Simba <rech16@student.aau.dk>
2020-10-05 14:48:15 +0200

    initial clustering randomwalk

--- cluster_random_walk.py
+++ cluster_random_walk.py
@@ -12,19 +12,19 @@ def cluster_page_rank(model: LdaModel, adj_matrix):
     adj_matrix = construct_transition_probability_matrix(adj_matrix)
     topic_clusters_plus_words = model.show_topics(lda.num_topics, formatted=False)
 
-    sen_scores = document_scores(adj_matrix, [], topic_clusters_plus_words)
-    return sen_scores
+    ranking_of_documents = document_scores(adj_matrix, [], topic_clusters_plus_words)
+    return ranking_of_documents
 
 
 def document_scores(adj_matrix, documents: List[str], topic_clusters, dmp_factor=0.85, num_steps=10):
     e = np.ones(adj_matrix.shape[0])
-    document_score = np.ones(len(documents))
+    document_score = np.ones(adj_matrix.shape[0])
 
     m_star = construct_new_transition_matrix(adj_matrix, documents, topic_clusters)
 
     for x in range(num_steps):
-        document_score = dmp_factor * np.dot(document_score, m_star.T) + (1 - dmp_factor) / len(documents) * e
-    return document_score
+        document_score = dmp_factor * np.dot(document_score, m_star.T) + (1 - dmp_factor) / adj_matrix.shape[0] * e
+    return document_score.argsort()[:][::-1]
 
 
 def construct_new_transition_matrix(adj_matrix,
@@ -43,32 +43,25 @@ def construct_new_transition_matrix(adj_matrix,
     :return: a transition matrix based on the cluster paper
     """
     # variable initialization
-    similarity_doc_clus = np.zeros(shape=(len(documents), len(clusters)))  # Document to Cluster
-    similarity_clus_doc_set = np.zeros(shape=(len(clusters),))  # Cluster to Document Set
-
-    # Calculate the similarity scores
-    for index, document in enumerate(documents):
-        similarity_doc_clus[index] = similarity_between_document_and_clusters(document, clusters)
-    for x in clusters:
-        similarity_clus_doc_set = similarity_between_cluster_and_document_set(clusters, documents)
-
-    # Normalize similarity vectors
-    similarity_clus_doc_set = similarity_clus_doc_set / similarity_clus_doc_set.sum(0)
-    similarity_doc_clus = similarity_doc_clus / similarity_doc_clus.sum(0)
+    doc_clus_sim = np.array(sp.load_npz("Generated Files/topic_doc_matrix.npz")[:1000].todense())  # Document to Cluster
+    clus_doc_set_sim = np.array(similarity_between_cluster_and_document_set(doc_clus_sim))  # Cluster to Document Set
 
     # Creating the new row normalized adjacency matrix
-    m_star = dmp_factor * np.dot(similarity_doc_clus, similarity_clus_doc_set) * adj_matrix + (1 - dmp_factor)
+    m_star = dmp_factor * np.dot(doc_clus_sim, clus_doc_set_sim) * np.array(adj_matrix) + (1 - dmp_factor)
     return m_star / m_star.sum(0)
 
 
-def similarity_between_document_and_clusters(sentence: str, cluster: dict) -> np.array:
-    return NotImplemented
+def similarity_between_document_and_clusters(document_index: int, cluster_index: int) -> np.array:
+    td_matrix = sp.load_npz("Generated Files/topic_doc_matrix.npz")
+    return td_matrix.getrow(document_index).getcol(cluster_index).data[0]
 
 
-def similarity_between_cluster_and_document_set(cluster: dict, document: net.Graph) -> np.array:
-    return NotImplemented
+def similarity_between_cluster_and_document_set(td_matrix) -> np.array:
+    return td_matrix.sum(axis=0) / td_matrix.shape[0]
 
 
 if __name__ == '__main__':
     lda = load_lda("LDA/model/docu_model")
-    construct_transition_probability_matrix(sp.load_npz("Generated Files/full_matrix.npz")[:1000, :1000])
+    adj_matrix = sp.load_npz("Generated Files/full_matrix.npz")[:1000, :1000]
+    ranks = cluster_page_rank(lda, adj_matrix)
+    print(ranks)
