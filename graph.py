774216c15ca101a4aec73ecf026738bab13fa82b
Peter Langballe Erichsen <perich16@student.aau.dk>
2020-10-06 11:37:23 +0200

    changed matrix type of td_matrix
    improved computation time.

--- graph.py
+++ graph.py
@@ -103,10 +103,8 @@ def stack_matrices_in_folder(path: str):
 
 if __name__ == '__main__':
     # Loading topic-document distribution matrix and initialisation
-    # convert to dok_matrix for faster lookup
-    # TODO test whether dok conversion improves computation speed, as both getcol() and lookups are used.
-    matrix = sp.dok_matrix(sp.load_npz("Generated Files/topic_doc_matrix.npz"))
-    doc_sim_chunker(matrix, 500, 8)
+    # whether csr_matrix or csc_matrix is faster will probably depend on the number of topics per document.
+    matrix = sp.csr_matrix(sp.load_npz("Generated Files/topic_doc_matrix.npz"))
 
     # Save full matrix
     sp.save_npz("Generated Files/full_matrix", stack_matrices_in_folder("Generated Files/adj/"))
