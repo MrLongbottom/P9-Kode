f02c03779408f5d629a0d8889ac416d3af140a7a
Simba <rech16@student.aau.dk>
2020-10-06 08:26:03 +0200

    added some refactoring

--- cluster_random_walk.py
+++ cluster_random_walk.py
@@ -1,67 +1,69 @@
-from typing import List, Dict
-
-from gensim.models import LdaModel
-
-from LDA.lda import load_lda
-from standard_random_walk import construct_transition_probability_matrix
 import numpy as np
 import scipy.sparse as sp
 
+from LDA.lda import load_lda
+from standard_random_walk import construct_transition_probability_matrix
 
-def cluster_page_rank(model: LdaModel, adj_matrix):
+
+def cluster_page_rank(adj_matrix: sp.csr_matrix) -> np.array:
     adj_matrix = construct_transition_probability_matrix(adj_matrix)
-    topic_clusters_plus_words = model.show_topics(lda.num_topics, formatted=False)
-
-    ranking_of_documents = document_scores(adj_matrix, [], topic_clusters_plus_words)
+    ranking_of_documents = document_scores(adj_matrix)
     return ranking_of_documents
 
 
-def document_scores(adj_matrix, documents: List[str], topic_clusters, dmp_factor=0.85, num_steps=10):
+def document_scores(adj_matrix, dmp_factor=0.85, num_steps=10):
+    """
+    Calculates the document scores for each document in the adj_matrix
+    by constructing the new transition probability matrix and random walking it
+    :param adj_matrix: adj_matrix
+    :param dmp_factor: 0.85
+    :param num_steps: number of power iterations
+    :return: sorted list of document based on their scores
+    """
     e = np.ones(adj_matrix.shape[0])
     document_score = np.ones(adj_matrix.shape[0])
 
-    m_star = construct_new_transition_matrix(adj_matrix, documents, topic_clusters)
+    m_star = construct_new_transition_matrix(adj_matrix)
 
     for x in range(num_steps):
         document_score = dmp_factor * np.dot(document_score, m_star.T) + (1 - dmp_factor) / adj_matrix.shape[0] * e
     return document_score.argsort()[:][::-1]
 
 
-def construct_new_transition_matrix(adj_matrix,
-                                    documents: List[str],
-                                    clusters: Dict[int, List[str]],
-                                    dmp_factor=0.85) -> np.array:
+def construct_new_transition_matrix(adj_matrix, dmp_factor=0.85) -> np.array:
     """
     This function construct the new transition matrix based on the cluster paper
     It calculates the similarity between
     Similarity(document -> document), Similarity(document -> cluster), Similarity(cluster -> document set)
     and combines into a matrix
     :param adj_matrix: adj matrix
-    :param documents: the documents
-    :param clusters: the cluster list
     :param dmp_factor: 0.85
     :return: a transition matrix based on the cluster paper
     """
-    # variable initialization
-    doc_clus_sim = np.array(sp.load_npz("Generated Files/topic_doc_matrix.npz")[:1000].todense())  # Document to Cluster
-    clus_doc_set_sim = np.array(similarity_between_cluster_and_document_set(doc_clus_sim))  # Cluster to Document Set
+    # Document to Cluster
+    doc_clus_sim = np.array(sp.load_npz("Generated Files/topic_doc_matrix.npz")[:1000].todense())
+
+    # Cluster to Document Set
+    clus_doc_set_sim = np.array(similarity_between_cluster_and_document_set(doc_clus_sim))
 
     # Creating the new row normalized adjacency matrix
     m_star = dmp_factor * np.dot(doc_clus_sim, clus_doc_set_sim) * np.array(adj_matrix) + (1 - dmp_factor)
+
+    # Returning the normalized version
     return m_star / m_star.sum(0)
 
 
-def similarity_between_document_and_clusters(document_index: int, cluster_index: int) -> np.array:
-    td_matrix = sp.load_npz("Generated Files/topic_doc_matrix.npz")
-    return td_matrix.getrow(document_index).getcol(cluster_index).data[0]
-
-
 def similarity_between_cluster_and_document_set(td_matrix) -> np.array:
+    """
+    Calculates the similarity between each cluster and the document set
+    :param td_matrix: topic-document matrix
+    :return: numpy array
+    """
     return td_matrix.sum(axis=0) / td_matrix.shape[0]
 
 
 if __name__ == '__main__':
     lda = load_lda("LDA/model/docu_model")
-    adj_matrix = sp.load_npz("Generated Files/full_matrix.npz")[:1000, :1000]
-    ranks = cluster_page_rank(lda, adj_matrix)
+    matrix = sp.load_npz("Generated Files/full_matrix.npz")[:1000, :1000]
+    ranks = cluster_page_rank(matrix)
     print(ranks)
