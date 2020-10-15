8434ecd1ee6ad82bc09c3500f6b439cb424871a6
Simba <rech16@student.aau.dk>
2020-10-15 09:49:11 +0200

    added new words found in 2017-2019

--- pipeline.py
+++ pipeline.py
@@ -4,11 +4,11 @@ from preprocessing import preprocess
 if __name__ == '__main__':
     # Setup pipeline for the project
     # Preprocess
-    cv_matrix, words, corpus = preprocess("documents.json")
+    cv_matrix, words, corpus = preprocess("data/data.json")
 
     # Run LDA
 
-    run_lda(path="LDA/model/document_model",
+    run_lda(path="LDA/model/full_document_model",
             cv_matrix=cv_matrix,
             words=words,
             corpus=corpus,
