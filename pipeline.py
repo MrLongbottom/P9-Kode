6b57837235a305847920033c8267466097d2ebd6
server <server>
2020-10-21 13:03:36 +0200

    New wik files and pipeline change

--- pipeline.py
+++ pipeline.py
@@ -4,11 +4,11 @@ from preprocessing import preprocess
 if __name__ == '__main__':
     # Setup pipeline for the project
     # Preprocess
-    cv_matrix, words, corpus = preprocess("documents.json")
+    cv_matrix, words, corpus = preprocess("data.json")
 
     # Run LDA
 
-    run_lda(path="LDA/model/document_model",
+    run_lda(path="LDA/model/full_model",
             cv_matrix=cv_matrix,
             words=words,
             corpus=corpus,
