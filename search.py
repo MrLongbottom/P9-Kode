9bf2d1a60d4fc8fc4d67ef9afc4aec280db347f8
Simba <rech16@student.aau.dk>
2020-11-03 12:03:54 +0100

    initial stuff

--- search.py
+++ search.py
@@ -1,12 +1,15 @@
-from typing import Dict
+from functools import partial
+from multiprocessing import Pool
+from typing import Dict, List
 
 import numpy as np
 import scipy.sparse as sp
 from gensim.corpora import Dictionary
+from tqdm import tqdm
 
 from lda import load_lda, get_document_topics_from_model, load_corpus
 from cluster_random_walk import cluster_page_rank
-from preprocessing import preprocess_query
+from preprocessing import preprocess_query, generate_queries, load_vector_file
 
 
 def make_personalization_vector(topics: Dict[int, float], topic_doc_matrix):
@@ -25,18 +28,61 @@ def query_topics(query: str, model_path: str, topic_doc_path) -> np.ndarray:
     return make_personalization_vector(q_topics, topic_doc_matrix)
 
 
+def load_doc_2_word(path, seperator=','):
+    with open(path, 'r') as file:
+        dictionary = {}
+        for line in file.readlines():
+            key = int(line.split(seperator)[0])
+            value = line.replace('[', '').replace(']', '').replace('\'', '').replace(' ', '').split(seperator)[1:][
+                0].split(',')
+            dictionary[key] = value
+    return dictionary
+
+
 def search(query: str, size_of_adj: int, lda_path: str, topic_doc_matrix_path: str) -> np.ndarray:
     search_vector = query_topics(query, lda_path, topic_doc_matrix_path)
     adj_matrix = sp.load_npz("Generated Files/full_matrix.npz")[:size_of_adj, :size_of_adj]
     return cluster_page_rank(adj_matrix, search_vector[:size_of_adj])
 
 
+def language_model(query: List[str], document_index: int):
+    p_wd = []
+    document = doc2word[document_index]
+    dirichlet_prior = sum([len(i) for i in list(doc2word.values())]) / len(doc2word)
+
+    for word in query:
+        word_index = inverse_w2v[word]
+        N_d = len(document)
+        tf = count_vectorizer[document_index, word_index]
+        w_freq_in_D = sum(count_vectorizer[:, word_index])
+        number_of_word_tokens = len(word2vec)
+        p_wd.append(
+            (N_d / (N_d + dirichlet_prior)) *
+            (tf / N_d) +
+            (1 - (N_d / (N_d + dirichlet_prior))) *
+            (w_freq_in_D / number_of_word_tokens))
+    return np.prod(p_wd)
+
+
 if __name__ == '__main__':
-    query1 = "fodbold spiller"
-    query2 = "katte i vand"
 
-    r_list1 = search(query1, 100, "LDA/model/docu_model_sqrt_div2", "Generated Files/topic_doc_matrix.npz")
-    r_list2 = search(query2, 100, "LDA/model/docu_model_sqrt_div2", "Generated Files/topic_doc_matrix.npz")
+    tf_idf_vec = sp.load_npz("Generated Files/tfidf_matrix.npz")
+    count_vectorizer = sp.load_npz("Generated Files/count_vec_matrix.npz")
 
-    print(r_list1[:10])
-    print(r_list2[:10])
+    doc2word = load_doc_2_word("Generated Files/doc2word.csv", '-')
+    word2vec = load_vector_file("Generated Files/word2vec.csv")
+    inverse_w2v = {v: k for k, v in word2vec.items()}
+
+    queries = generate_queries(tf_idf_vec, word2vec, 10, 4)
+    query_words = list(queries.items())[0][1].split(' ')
+    query_index = list(queries.items())[0][0]
+
+    lst = []
+    with Pool(processes=8) as p:
+        max_ = count_vectorizer.shape[0]
+        with tqdm(total=max_) as pbar:
+            for i, score in enumerate(p.imap_unordered(partial(language_model, query_words), range(0, max_))):
+                lst.append((i, score))
+                pbar.update()
+    sorted_list = list(dict(sorted(lst, key=lambda x: x[1], reverse=True)).keys())
+    print(f"index of document: {sorted_list.index(query_index)}")
