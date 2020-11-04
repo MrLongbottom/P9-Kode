35f220abec1ccb578af1a4e02a79e7414f164676
Dennis Højbjerg Rose <drose16@student.aau.dk>
2020-11-04 11:13:51 +0100

    Load model and corpus.

---
+++ visualization.py
@@ -0,0 +1,6 @@
+from lda import load_lda, load_corpus
+
+if __name__ == '__main__':
+    pass
+    # lda_model = load_lda("LDA/model/full_model")
+    # corpus = load_corpus("Generated Files/corpus")
