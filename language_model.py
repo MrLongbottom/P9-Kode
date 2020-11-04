5f2cc7eae59222d9fabc629dd917f7b9fbd5c165
MrLongbottom <perich16@student.aau.dk>
2020-11-04 13:57:27 +0100

    split up language model calculations in 3

--- language_model.py
+++ language_model.py
@@ -17,41 +17,53 @@ inverse_w2v = {v: k for k, v in word2vec.items()}
 dirichlet_prior = sum([len(i) for i in list(doc2word.values())]) / len(doc2word)
 
 
-def language_model(query: List[str], document_index: int):
+def lm_evaluate_word_doc(word:str, doc, doc_id):
+    word_index = inverse_w2v[word]
+    N_d = len(doc)
+    tf = count_vectorizer[doc_id, word_index]
+    w_freq_in_D = np.sum(count_vectorizer[:, word_index])
+    number_of_word_tokens = len(word2vec)
+    return np.prod(
+        [(N_d / (N_d + dirichlet_prior)),
+         (tf / N_d) + (1 - (N_d / (N_d + dirichlet_prior))),
+         (w_freq_in_D / number_of_word_tokens)])
+
+
+def lm_evaluate_query_doc(query: List[str], document_index: int):
     p_wd = []
     document = doc2word[document_index]
-
     for word in query:
-        word_index = inverse_w2v[word]
-        N_d = len(document)
-        tf = count_vectorizer[document_index, word_index]
-        w_freq_in_D = np.sum(count_vectorizer[:, word_index])
-        number_of_word_tokens = len(word2vec)
-        p_wd.append(np.prod(
-            [(N_d / (N_d + dirichlet_prior)),
-             (tf / N_d) + (1 - (N_d / (N_d + dirichlet_prior))),
-             (w_freq_in_D / number_of_word_tokens)]))
+        p_wd.append(lm_evaluate_word_doc(word, document, document_index))
     return np.prod(p_wd)
 
 
-if __name__ == '__main__':
-    queries = query_handling.generate_queries(count_vectorizer, word2vec, 10)
-    query_words = list(queries.items())[0][1].split(' ')
-    query_index = list(queries.items())[0][0]
-
-    print(f"query: {query_words}")
+def lm_evaluate_query(query_index, query_words, tell=False):
     lst = {}
     with Pool(processes=8) as p:
         max_ = count_vectorizer.shape[0]
         with tqdm(total=max_) as pbar:
-            for i, score in enumerate(p.imap(partial(language_model, query_words), range(0, max_))):
+            for i, score in enumerate(p.imap(partial(lm_evaluate_query_doc, query_words), range(0, max_))):
                 lst[i] = score
                 pbar.update()
 
     sorted_list = list(dict(sorted(lst.items(), key=lambda x: x[1], reverse=True)).keys())
-    print(f"index of document: {sorted_list.index(query_index)}")
+    if tell:
+        print(f"query: {query_words}")
+        print(f"index of document: {sorted_list.index(query_index)}")
+        print(f"number 1: {doc2word[sorted_list[0]]}\n")
+        print(f"number 2: {doc2word[sorted_list[1]]}\n")
+        print(f"number 3: {doc2word[sorted_list[2]]}\n")
+        print(f"real document: {doc2word[query_index]}")
+    return sorted_list.index(query_index)
+
+
+if __name__ == '__main__':
+    queries = query_handling.generate_queries(count_vectorizer, word2vec, 10, min_length=1, max_length=4)
+    query_words = list(queries.items())[0][1].split(' ')
+    query_index = list(queries.items())[0][0]
+
     print(f"query: {query_words}")
-    print(f"number 1: {doc2word[sorted_list[0]]}\n")
-    print(f"number 2: {doc2word[sorted_list[1]]}\n")
-    print(f"number 3: {doc2word[sorted_list[2]]}\n")
-    print(f"real document: {doc2word[query_index]}")
+    res = lm_evaluate_query(query_index, query_words, tell=True)
+
+
+
