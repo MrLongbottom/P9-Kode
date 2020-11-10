e6984adbf0de6c54cdab215ab9c9d746a1fbe221
Simba <rech16@student.aau.dk>
2020-11-10 12:49:29 +0100

    making a running version but the lda_evaluate can return duplicate documents

--- combinations.py
+++ combinations.py
@@ -1,13 +1,16 @@
 import language_model
 import query_handling
 import utility
-import numpy as np
+import scipy.sparse as sp
+
 
 if __name__ == '__main__':
     queries = utility.load_vector_file("Generated Files/queries.csv")
     query_key = list(queries.keys())[0]
     query_content = list(queries.values())[0].split(' ')
-    res1, p_vec1 = query_handling.lda_evaluate_query(query_key, query_content, tell=True)
+    dt = sp.load_npz("Generated Files/topic_doc_matrix.npz")
+    tw = sp.load_npz("Generated Files/topic_word_matrix.npz")
+    res1, p_vec1 = query_handling.lda_evaluate_query(query_key, query_content, tell=True, dt_matrix=dt, tw_matrix=tw)
     res2, p_vec2 = language_model.lm_evaluate_query(query_key, query_content, tell=True)
     p_vec3 = [p_vec1[i] * p_vec2[i] for i in range(len(p_vec1))]
     print(str(res1) + " " + str(res2))
