5861f783df9a08713bde5613cb61695cc0156b1c
Simba <rech16@student.aau.dk>
2020-11-10 13:20:43 +0100

    added the combination which seems to give pretty good results

--- combinations.py
+++ combinations.py
@@ -2,16 +2,18 @@ import language_model
 import query_handling
 import utility
 import scipy.sparse as sp
+import numpy as np
 
 
 if __name__ == '__main__':
     queries = utility.load_vector_file("Generated Files/queries.csv")
-    query_key = list(queries.keys())[0]
-    query_content = list(queries.values())[0].split(' ')
+    query_key = list(queries.keys())[2]
+    query_content = list(queries.values())[2].split(' ')
     dt = sp.load_npz("Generated Files/topic_doc_matrix.npz")
     tw = sp.load_npz("Generated Files/topic_word_matrix.npz")
-    res1, p_vec1 = query_handling.lda_evaluate_query(query_key, query_content, tell=True, dt_matrix=dt, tw_matrix=tw)
-    res2, p_vec2 = language_model.lm_evaluate_query(query_key, query_content, tell=True)
-    p_vec3 = [p_vec1[i] * p_vec2[i] for i in range(len(p_vec1))]
-    print(str(res1) + " " + str(res2))
-    print(f"lda: {p_vec1[query_key]}, lm: {p_vec2[query_key]}, combo: {p_vec3[query_key]}")
+    res1, p_vec1 = query_handling.lda_evaluate_query(query_key, query_content, tell=False, dt_matrix=dt, tw_matrix=tw)
+    res2, p_vec2 = language_model.lm_evaluate_query(query_key, query_content, tell=False)
+    p_vec3 = {k: p_vec1[k]*p_vec2[k] for k in range(len(p_vec1))}
+    res3 = list(dict(sorted(p_vec3.items(), key=lambda x: x[1], reverse=True)).keys()).index(query_key)
+    print(f" LDA index: {res1}, LM index:{res2} LDA*LM index:{res3}")
+    print(f"LDA index value: {p_vec1[query_key]}, LM index value: {p_vec2[query_key]}, LDA*LM index value: {p_vec3[query_key]}")
