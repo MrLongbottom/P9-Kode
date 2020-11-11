411c4cee5da504b4babe6ce22c1e9148c8ee8687
Simba <rech16@student.aau.dk>
2020-11-11 12:06:26 +0100

    giant speedup

--- query_evaluation.py
+++ query_evaluation.py
@@ -6,6 +6,7 @@ import utility
 cv_matrix = sp.load_npz("Generated Files/count_vec_matrix.npz")
 dt_matrix = sp.load_npz("Generated Files/topic_doc_matrix.npz")
 tw_matrix = sp.load_npz("Generated Files/topic_word_matrix.npz")
+wordfreq = sp.load_npz("Generated Files/word_freq.npz")
 doc2word = utility.load_vector_file("Generated Files/doc2word.csv")
 word2vec = utility.load_vector_file("Generated Files/word2vec.csv")
 dirichlet_smoothing = sum([len(i) for i in list(doc2word.values())]) / len(doc2word)
@@ -33,10 +34,11 @@ def lm_evaluate_word_doc(document_index, word_index):
     """
     N_d = len(doc2word[document_index])
     tf = cv_matrix[document_index, word_index]
-    w_freq_in_D = np.sum(cv_matrix[:, word_index])
+    w_freq_in_D = wordfreq[word_index].data[0]
     number_of_word_tokens = len(word2vec)
-    score = np.prod([(N_d / (N_d + dirichlet_smoothing)), (tf / N_d)]) + \
-           np.prod([(1 - (N_d / (N_d + dirichlet_smoothing))), (w_freq_in_D / number_of_word_tokens)])
+    score = ((N_d / (N_d + dirichlet_smoothing)) * (tf / N_d)) + \
+            ((1 - (N_d / (N_d + dirichlet_smoothing))) * (
+                w_freq_in_D / number_of_word_tokens))
     return score
 
 
