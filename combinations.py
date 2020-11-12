13f5761f4c2d388504b8ebdc5d831ccce3660624
Simba <rech16@student.aau.dk>
2020-11-12 14:43:26 +0100

    made combinations work as well

--- combinations.py
+++ combinations.py
@@ -1,4 +1,5 @@
 import utility
+import scipy.sparse as sp
 from query_handling import evaluate_query
 from query_evaluation import lm_evaluate_word_doc, lda_evaluate_word_doc, lm_lda_combo_evaluate_word_doc
 
@@ -23,12 +24,18 @@ def investigate_results():
 
 
 def lm_lda_combo():
-    # Load queries
+    # Load queries and matrices
     queries = utility.load_vector_file("Generated Files/queries.csv")
+    dt_matrix = sp.load_npz("Generated Files/topic_doc_matrix.npz")
+    tw_matrix = sp.load_npz("Generated Files/topic_word_matrix.npz")
 
     for query_key, query_content in queries.items():
         # Evaluate the queries
-        res, p_vec = evaluate_query(lm_lda_combo_evaluate_word_doc, query_key, query_content.split(' '), tell=False)
+        res, p_vec = evaluate_query(lm_lda_combo_evaluate_word_doc,
+                                    query_key,
+                                    query_content.split(' '),
+                                    (dt_matrix, tw_matrix),
+                                    tell=False)
         # List the results
         print(f"original query index: {query_key} original query: {query_content}")
         print(f"LDA*LM index:{res}")
